300e9f187ed961ad12b01955175f2a6821da0d1a
Marina Onohara <marina.onohar@gmail.com>
2026-07-06 17:26:24 -0300

    Deploy do modelo em produção - Construção da API

--- API/handler.py
+++ API/handler.py
@@ -4,7 +4,7 @@ from flask import Flask, request, Response
 from rossmann.Rossmann import Rossmann
 
 # loading model
-model = pickle.load( open( 'C:/repos/Rossmann_Project/Model/model_rossmann.pkl', 'rb') )
+model = pickle.load( open( 'Model/model_rossmann.pkl', 'rb') )
 
 # initialize API
 app = Flask( __name__ )
